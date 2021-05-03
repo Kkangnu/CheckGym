f8befae14078739925dc98831e3adb320a5c2d0c
Rhee <lkd0527@gmail.com>
2021-05-03 23:00:35 +0900

    Main화면 이미지 추가 키보드 화면 구현

--- app/src/main/java/net/onerm/checkgym/kiosk/activity/KioskMainActivity.kt
+++ app/src/main/java/net/onerm/checkgym/kiosk/activity/KioskMainActivity.kt
@@ -1,43 +1,75 @@
 package net.onerm.checkgym.kiosk.activity
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import androidx.viewpager2.widget.ViewPager2
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import net.onerm.checkgym.R
 import net.onerm.checkgym.const.ACT_MODE
 import net.onerm.checkgym.const.CheckGymConsts
+import net.onerm.checkgym.kiosk.adapter.KioskAdsAdapter
 import net.onerm.checkgym.kiosk.model.KioskNoticeModel
-import net.onerm.checkgym.kiosk.model.KioskRequestAudio
 import net.onerm.checkgym.kiosk.model.KioskRequestModel
 import net.onerm.checkgym.service.CheckGymApi
-import java.lang.reflect.Type
 
 
 class KioskMainActivity : AppCompatActivity() {
+    lateinit var compositeDisposable: CompositeDisposable
+    lateinit var viewPagerImageSlider: ViewPager2
+    lateinit var kioskAdsAdpater: KioskAdsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kiosk_main)
 
         initView()
+        initData()
+    }
+
+    private fun initData(){
+        // Get광고
+        getAds1()
     }
 
     private fun initView(){
-
+        // 광고
+        viewPagerImageSlider = findViewById(R.id.viewPagerImageSlider)
     }
 
+    // 광고1
+    private fun getAds1() {
+        var kioskRequestModel = KioskRequestModel()
+        kioskRequestModel.actmode = ACT_MODE.GET_ADS_1.action
+        kioskRequestModel.comno = CheckGymConsts.API_VALUE_COMNO
+
+        makeKioskDispose(kioskRequestModel)
+    }
+
+    private fun makeKioskDispose (kioskRequestModel: KioskRequestModel) {
+        compositeDisposable = CompositeDisposable()
+
+        compositeDisposable.add(CheckGymApi.postKiosk(kioskRequestModel)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribe({ response: KioskNoticeModel ->
+                    Log.d("MainActivity", response.toString())
+
+                    if(response.rows != null) {
+                        kioskAdsAdpater = KioskAdsAdapter(this, response.rows, viewPagerImageSlider)
+                        viewPagerImageSlider.adapter = kioskAdsAdpater
+                    }
+                }, { error: Throwable ->
+                    Log.d("MainActivity", error.localizedMessage)
+                    Toast.makeText(this, "Error ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+                })
+        )
+    }
+
+    
     override fun onDestroy() {
         super.onDestroy()
     }
